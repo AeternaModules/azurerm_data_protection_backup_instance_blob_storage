bc0ee99984cf1dd72f8ad559ece3b204419b2163
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:40:59 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "data_protection_backup_instance_blob_storages" {
+  description = "All data_protection_backup_instance_blob_storage resources"
+  value       = azurerm_data_protection_backup_instance_blob_storage.data_protection_backup_instance_blob_storages
+}
+
